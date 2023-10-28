be034950afb7ea358d88bc71d5cb706491a328a6
Darya Bugaets <40bugaets@mail.ru>
2023-10-28 21:39:47 +0300

    1lec

---
+++ main.py
@@ -0,0 +1,53 @@
+# print(5, 8, 6)
+# n=5
+# print(n)
+# n='fgdf'
+# print(n)
+# n1="hyodf"
+
+# n=5
+# print(type(n))
+
+# n='sdfp\'gfp'
+# print(n)
+
+# a=5
+# b=5.48
+# c='hello'
+# print(a,b,c)
+
+# a=5
+# b=5.48
+# c='hello'
+# print(a,'-',b,'-',c,'-')
+
+# a=5
+# b=5.48
+# c='hello'
+# print(f'{a}-{b}-{c}')
+
+# a=5
+# b=5.48
+# c='hello'
+# print("{}-{}-{}".format(a,b,c))
+
+# print('введите первое число:')
+# a=input()
+# b=input('введите второе число: ')
+
+# print('введите первое число:')
+# a=int(input())
+# b=int(input('введите второе число: '))
+# print(a,'+',b,'=',a+b)
+
+username=input('Введите имя: ')
+if username == 'Маша':
+    print('Ура, это же Маша!')
+elif username == 'Марина':
+    print('Я так ждала Вас, Марина!')
+elif username == 'Ильнар':
+    print('Ильнар, урааа!)')
+else:
+    print('Привет, ', username)
+
+    
